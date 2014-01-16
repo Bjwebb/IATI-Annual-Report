4f9e5121c9e932d422a2e4a35eeb8fc4a0868b7a
Ben Webb <bjwebb67@googlemail.com>
2014-01-16 13:17:34 +0000

    Switch to using json files directly

--- add_transparency_indicator.py
+++ add_transparency_indicator.py
@@ -1,15 +1,6 @@
-import os, csv
+import os, json, csv
 from collections import defaultdict
 import urllib.parse
-
-publishers = defaultdict(dict)
-
-for sheetname in ['Other.csv', 'Signatory.csv']:
-    reader = csv.reader(open(sheetname))
-    header = reader.__next__()
-    for row in reader:
-        for i, cell in enumerate(row):
-            publishers[header[i]][row[1]] = cell
 
 line_mapping = {
     #11: '1.1',
@@ -20,32 +11,37 @@ line_mapping = {
     25: '2.3',
     27: '2.4',
     29: '2.5',
-    31: '3.1',
-    33: '3.3',
+    31: '3.1.1',
+    33: '3.2',
     35: '5.1',
     37: '5.2',
     39: '5.3',
     41: '6.1',
     43: '6.2',
-    45: '6.3'
+    45: '6.3.1'
 }
 
+publisher_reverse = {}
+j = json.load(open('publishers.json'))
+for publisher in j['result']:
+    publisher_reverse[publisher['display_name']] = publisher['name']
+
 for fname in os.listdir('in'):
-    #name = urllib.parse.unquote(fname.split('-')[-1]).strip(' ')[:-4]
-    #if name == 'UN Habitat': name = 'UN-Habitat'
     reader = csv.reader(open(os.path.join('in',fname)))
     try:
         header = reader.__next__()
     except StopIteration: continue
     name = header[0]
-    if name in publishers:
+    if name in publisher_reverse:
+        slug = publisher_reverse[name]
+        publisher_tests = json.load(open('results/{0}.json'.format(slug)))['tests']
         writer = csv.writer(open(os.path.join('out',name+'.csv'), 'w'))
         for i, row in enumerate(reader):
             try:
                 if i in line_mapping:
-                    row[1] = publishers[name][line_mapping[i]]
-            except KeyError:
-                print('Stats missing for {0}'.format(name))
+                    row[1] = publisher_tests[line_mapping[i]]['percentage']
+            except KeyError as e:
+                print('{0} missing for {1}'.format(e, name))
             writer.writerow(row)
     else:
         print('{0} does not match'.format(name))
--- get.sh
+++ get.sh
@@ -1,3 +1,6 @@
 mkdir in
 cd in
 for i in {0..60}; do wget --load-cookies ../cookies.txt --content-disposition "https://docs.google.com/spreadsheet/fm?id=tpNjubrRwAnx2osheCEnZ_A.00522594596467399731.3531356485886094335&fmcmd=5&gid=$i"; done  
+
+
+curl "http://iatiregistry.org/api/3/action/organization_list?all_fields=true" > publishers.json
