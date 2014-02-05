df4340e107f75e2c7b1564af64bb60fe8e6f6d06
Ben Webb <bjwebb67@googlemail.com>
2014-02-05 11:19:25 +0000

    Add missing fields

--- add_transparency_indicator.py
+++ add_transparency_indicator.py
@@ -3,9 +3,11 @@ from collections import defaultdict
 import urllib.parse
 
 line_mapping = {
-    #11: '1.1',
+    11: '1.1',
     #13:
-    #15: '1.3',
+    15: '1.3',
+    17: '1.4.1',
+    19: '1.4.2',
     21: '2.1',
     23: '2.2',
     25: '2.3',
@@ -26,6 +28,25 @@ j = json.load(open('publishers.json'))
 for publisher in j['result']:
     publisher_reverse[publisher['display_name']] = publisher['name']
 
+extra_tests = defaultdict(dict)
+
+reader = csv.reader(open('csv/Timeliness_Transactions_1.1.csv'))
+for row in reader:
+    if len(row) > 20:
+        extra_tests[row[0]]['1.1'] = row[20]
+reader = csv.reader(open('csv/Activity_planning_3.csv'))
+for row in reader:
+    if len(row) > 4:
+        extra_tests[row[0]]['1.3'] = row[4]
+reader = csv.reader(open('csv/Alignement_with_financial_year_Transactions_4.1.csv'))
+for row in reader:
+    if len(row) > 24:
+        extra_tests[row[0]]['1.4.1'] = row[24]
+reader = csv.reader(open('csv/Alignement_with_financial_year_Budgets_4.2.csv'))
+for row in reader:
+    if len(row) > 11:
+        extra_tests[row[0]]['1.4.2'] = row[11]
+
 for fname in os.listdir('in'):
     reader = csv.reader(open(os.path.join('in',fname)))
     try:
@@ -37,11 +58,14 @@ for fname in os.listdir('in'):
         publisher_tests = json.load(open('results/{0}.json'.format(slug)))['tests']
         writer = csv.writer(open(os.path.join('out',name+'.csv'), 'w'))
         for i, row in enumerate(reader):
-            try:
-                if i in line_mapping:
-                    row[1] = publisher_tests[line_mapping[i]]['percentage']
-            except KeyError as e:
-                print('{0} missing for {1}'.format(e, name))
+            if i in line_mapping:
+                test = line_mapping[i]
+                if test in publisher_tests:
+                    row[1] = publisher_tests[test]['percentage']
+                elif test in extra_tests[slug]:
+                    row[1] = extra_tests[slug][test]
+                else:
+                    print('{0} missing for {1}'.format(test, name))
             writer.writerow(row)
     else:
         print('{0} does not match'.format(name))
